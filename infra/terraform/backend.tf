d882b1de9c50a5c4b905947042bfda8afe968ecf
Adrian Szarnowski <adrianszarnowski8@gmail.com>
2023-08-14 17:00:14 +0200

    chore: renamed repo

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "terraform-state-20230804212028694000000001"
-    key     = "ss-monitors/terraform.tfstate"
+    key     = "ss-bot/terraform.tfstate"
     region  = "eu-west-1"
     encrypt = true
   }
--- infra/terraform/oidc_github.tf
+++ infra/terraform/oidc_github.tf
@@ -1,6 +1,6 @@
 module "aws_oidc_github" {
-  source        = "github.com/TheSoftwareHouse/terraform-modules.git//aws_oidc_github?ref=main"
-  role_name     = "github-actions"
-  github_org    = "Nynu8"
-  github_repos  = ["SS-monitors"]
+  source       = "github.com/TheSoftwareHouse/terraform-modules.git//aws_oidc_github?ref=main"
+  role_name    = "github-actions"
+  github_org   = "Nynu8"
+  github_repos = ["SS-bot"]
 }
