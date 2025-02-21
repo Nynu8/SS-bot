6fb94067d36bf168afaafd3bf62de8afd20492a8
Adrian Szarnowski <adrianszarnowski8@gmail.com>
2025-02-21 14:40:42 +0100

    fix: ignore invalid cert

--- shared/ss-website/fetch-players-from-website.ts
+++ shared/ss-website/fetch-players-from-website.ts
@@ -1,9 +1,14 @@
+import * as https from "https";
 import axios from "axios";
 import { getPlayersFromHtml } from "./get-players-from-html";
 import { PlayerInfo } from "./types";
 
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false,
+});
+
 export const fetchPlayersFromWebsite = async (url: string): Promise<PlayerInfo[]> => {
-  const response = await axios.get(url);
+  const response = await axios.get(url, { httpsAgent });
 
   return getPlayersFromHtml(response.data);
 };
